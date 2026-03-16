d0bd37d9c8c6d01500d154900cc1b78a5b42ecc0
syauqy <syauqyaziz@gmail.com>
2026-03-16 16:16:22 +0700

    Add Ahrefs analytics script to document head

--- pages/_document.js
+++ pages/_document.js
@@ -3,11 +3,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-      ></meta>
+      <Head>
+        <script
+          src="https://analytics.ahrefs.com/analytics.js"
+          data-key="17Qf8HVhOW6orqywwIiksg"
+          async
+        ></script>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
